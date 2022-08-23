f3d1cd45e96e570e950e83d2efcb09dea4fd416b
rahul224 <rahul.ranjan423@gmail.com>
2022-08-23 09:47:42 +0530

    'Init'

---
+++ public/register.php
@@ -0,0 +1,11 @@
+<?php  
+require_once __DIR__."/../src/bootstrap.php";
+
+view('header', ['title'=>'Register']);
+?>
+
+<form action="register.php" method="POST"> 
+	
+</form>
+
+<?php view('footer'); ?>
---
+++ src/inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-widht;inital-scale=1.0">
+	<title><?= $title ?? 'Home' ?></title>
+	<link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+</head>
+<body>
+	<main>
+<?php flash(); ?>
